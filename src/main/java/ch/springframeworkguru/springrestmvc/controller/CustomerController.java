7722e88a58b33c7fc492336e097e96f2e8b4f910
dboeckli <dboeckli@gmail.com>
2024-09-05 15:31:11 +0200

    dry

--- src/main/java/ch/springframeworkguru/springrestmvc/controller/CustomerController.java
+++ src/main/java/ch/springframeworkguru/springrestmvc/controller/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/customer")
+@RequestMapping("${controllers.customer-controller.request-path}")
 @Slf4j
 public class CustomerController {
 
