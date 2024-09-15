d841a5ae5711dae72375c934225b34a57e9221b1
dboeckli <dboeckli@gmail.com>
2024-09-15 15:30:42 +0200

    implement controller binding validation

--- src/test/java/ch/springframeworkguru/springrestmvc/controller/BeerControllerTest.java
+++ src/test/java/ch/springframeworkguru/springrestmvc/controller/BeerControllerTest.java
@@ -3,7 +3,9 @@ package ch.springframeworkguru.springrestmvc.controller;
 import ch.springframeworkguru.springrestmvc.service.BeerService;
 import ch.springframeworkguru.springrestmvc.service.BeerServiceImpl;
 import ch.springframeworkguru.springrestmvc.service.dto.BeerDTO;
+import ch.springframeworkguru.springrestmvc.service.dto.BeerStyle;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +15,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -92,12 +98,74 @@ class BeerControllerTest {
         given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(givenBeer);
 
         mockMvc.perform(post(requestPath + "/createBeer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(givenBeer)))
+            .andExpect(status().isCreated())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+    }
+
+    @Test
+    void testCreateBeerNullBeername() throws Exception {
+        BeerDTO givenBeer = BeerDTO.builder()
+            .beerName(null)
+            .beerStyle(BeerStyle.PALE_ALE)
+            .upc("123")
+            .price(BigDecimal.valueOf(5))
+            .build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(givenBeer);
+
+        MvcResult mvcResult = mockMvc.perform(post(requestPath + "/createBeer")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(givenBeer)))
-                .andExpect(status().isCreated())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(status().isBadRequest()).andReturn();
 
+        assertThat(mvcResult.getResponse().getContentAsString(), Matchers
+            .either(Matchers.is("[{beerName=must not be blank}, {beerName=must not be null}]"))
+                .or(Matchers.is("[{beerName=must not be null}, {beerName=must not be blank}]")));
+    }
+
+    @Test
+    void testCreateBeerEmptyBeername() throws Exception {
+        BeerDTO givenBeer = BeerDTO.builder()
+            .beerName("")
+            .beerStyle(BeerStyle.PALE_ALE)
+            .upc("123")
+            .price(BigDecimal.valueOf(5))
+            .build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(givenBeer);
+
+        MvcResult mvcResult = mockMvc.perform(post(requestPath + "/createBeer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(givenBeer)))
+            .andExpect(status().isBadRequest()).andReturn();
+
+        assertEquals("[{beerName=must not be blank}]", mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testCreateBeerBlankBeername() throws Exception {
+        BeerDTO givenBeer = BeerDTO.builder()
+            .beerName("          ")
+            .beerStyle(BeerStyle.PALE_ALE)
+            .upc("123")
+            .price(BigDecimal.valueOf(5))
+            .build();
+
+        given(beerService.saveNewBeer(any(BeerDTO.class))).willReturn(givenBeer);
+
+        MvcResult mvcResult = mockMvc.perform(post(requestPath + "/createBeer")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(givenBeer)))
+            .andExpect(status().isBadRequest()).andReturn();
+
+        assertEquals("[{beerName=must not be blank}]", mvcResult.getResponse().getContentAsString());
     }
 
     @Test
@@ -114,6 +182,25 @@ class BeerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json(objectMapper.writeValueAsString(givenBeerToEdit)));  // oder das ganze Object;
+    }
+
+    @Test
+    void testEditBeerNullBeername() throws Exception {
+        BeerDTO givenBeerToEdit = beerServiceImpl.listBeers().getFirst();
+        givenBeerToEdit.setBeerName(null);
+
+        given(beerService.editBeer(givenBeerToEdit.getId(), givenBeerToEdit)).willReturn(Optional.of(givenBeerToEdit));
+
+        MvcResult mvcResult = mockMvc.perform(put(requestPath + "/editBeer/" + givenBeerToEdit.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(givenBeerToEdit)))
+            .andExpect(status().isBadRequest()).andReturn();
+
+       
+        assertThat(mvcResult.getResponse().getContentAsString(), Matchers
+            .either(Matchers.is("[{beerName=must not be blank}, {beerName=must not be null}]"))
+            .or(Matchers.is("[{beerName=must not be null}, {beerName=must not be blank}]")));
     }
 
     @Test
