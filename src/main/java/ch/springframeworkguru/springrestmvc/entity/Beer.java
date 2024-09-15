fe7cb4c5ab770022322fec1c43fe74dace46a0bf
dboeckli <dboeckli@gmail.com>
2024-09-15 19:39:46 +0200

    implement jpa validation validation

--- src/main/java/ch/springframeworkguru/springrestmvc/entity/Beer.java
+++ src/main/java/ch/springframeworkguru/springrestmvc/entity/Beer.java
@@ -2,6 +2,9 @@ package ch.springframeworkguru.springrestmvc.entity;
 
 import ch.springframeworkguru.springrestmvc.service.dto.BeerStyle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -26,11 +29,24 @@ public class Beer {
     @Version
     private Integer version;
 
+    @NotNull
+    @NotBlank
+    @Size(max=50)
+    @Column(length = 50, nullable = false)
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
     private String upc;
+    
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
+    
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
 }
--- src/test/java/ch/springframeworkguru/springrestmvc/repository/BeerRepositoryTest.java
+++ src/test/java/ch/springframeworkguru/springrestmvc/repository/BeerRepositoryTest.java
@@ -1,9 +1,13 @@
 package ch.springframeworkguru.springrestmvc.repository;
 
 import ch.springframeworkguru.springrestmvc.entity.Beer;
+import ch.springframeworkguru.springrestmvc.service.dto.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,9 +19,14 @@ class BeerRepositoryTest {
 
     @Test
     void testSave() {
-        Beer savedBeer = beerRepository.save(Beer.builder().
-                beerName("hallo")
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                .beerName("hallo")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123")
+                .price(BigDecimal.valueOf(10))
                 .build());
+
+        beerRepository.flush();
 
         assertAll(
                 () -> assertNotNull(savedBeer),
@@ -25,6 +34,21 @@ class BeerRepositoryTest {
                 () -> assertNotNull(savedBeer.getBeerName()),
                 () -> assertEquals("hallo", savedBeer.getBeerName())
         );
+    }
+
+    @Test
+    void testSaveBeerNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                .beerName("hallo 01234567890123456789012345678901234567890123456789")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123")
+                .price(BigDecimal.valueOf(10))
+                .build());
+
+            beerRepository.flush();
+        });
+
     }
 
 }
