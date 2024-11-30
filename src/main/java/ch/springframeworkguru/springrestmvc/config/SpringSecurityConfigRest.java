79aa486c314576433195ab1959bec7c6b96b1439
Dominique Boeckli <dboeckli@gmail.com>
2024-11-30 10:18:02 +0100

    validate with openapi spec (#23)
    * validate with openapi spec
    
    * add a TODO

--- src/main/java/ch/springframeworkguru/springrestmvc/config/SpringSecurityConfigRest.java
+++ src/main/java/ch/springframeworkguru/springrestmvc/config/SpringSecurityConfigRest.java
@@ -2,11 +2,13 @@ package ch.springframeworkguru.springrestmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Profile("!test-disabled-security")
 public class SpringSecurityConfigRest {
 
     /* http basic auth ./
