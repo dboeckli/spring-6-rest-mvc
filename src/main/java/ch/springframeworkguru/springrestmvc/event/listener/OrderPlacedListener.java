6b26c14e7ae8b4221cc032694d785ae2201443d8
Dominique Boeckli <dboeckli@gmail.com>
2024-12-22 09:49:04 +0100

    Feature/continue with kafka (#35)
    * add kafka dependency
    
    * add kafkalistener
    
    * implement splitter
    
    * IMPLEMENT TASK

---
+++ src/main/java/ch/springframeworkguru/springrestmvc/event/listener/OrderPlacedListener.java
@@ -0,0 +1,31 @@
+package ch.springframeworkguru.springrestmvc.event.listener;
+
+import ch.guru.springframework.spring6restmvcapi.events.OrderPlacedEvent;
+import ch.springframeworkguru.springrestmvc.config.KafkaConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class OrderPlacedListener {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Async
+    @EventListener
+    public void listen(OrderPlacedEvent event) {
+        log.info("Beer Order Event Listener called for event: " + event.getClass().getName());
+        log.info("Beer Order Event Listener called for event: {}", event.getBeerOrderDTO().getId());
+        log.info("Current Thread name: " + Thread.currentThread().getName());
+        log.info("Current Thread ID: " + Thread.currentThread().threadId());
+        // TODO: IMPLEMENT PROCESS ORDER LOGIC HERE. SEND TO KAFKA
+
+        kafkaTemplate.send(KafkaConfig.ORDER_PLACED_TOPIC, event);
+    }    
+    
+}
