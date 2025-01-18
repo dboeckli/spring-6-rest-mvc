5344d5cbff2670607c121e7c0bd075b2c61f05bb
Dominique Boeckli <dboeckli@gmail.com>
2025-01-18 12:05:38 +0100

    Feature/fix dependentbot (#53)
    * add personal access token to dependent bot
    
    * add sonar build
    
    * fix unit test

--- src/test/java/ch/springframeworkguru/springrestmvc/SpringRestMvcApplicationTests.java
+++ src/test/java/ch/springframeworkguru/springrestmvc/SpringRestMvcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @ActiveProfiles("test")
 class SpringRestMvcApplicationTests {
 
