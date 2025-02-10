53a6cb6b38e359716fc6638877021e40ae2b19b8
Dominique Boeckli <dboeckli@gmail.com>
2025-02-10 09:36:25 +0100

    fix unit test (#71)

--- src/test/java/ch/springframeworkguru/springrestmvc/SpringRestMvcApplicationTests.java
+++ src/test/java/ch/springframeworkguru/springrestmvc/SpringRestMvcApplicationTests.java
@@ -1,15 +1,26 @@
 package ch.springframeworkguru.springrestmvc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @ActiveProfiles("test")
+@Slf4j
 class SpringRestMvcApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext, "Application context should not be null");
+        log.info("Testing Spring 6 Application {}", applicationContext.getApplicationName());
     }
 
 }
